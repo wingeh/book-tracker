edf40ba8aa1d3d3a944d59c7f6f98c250fa81797
Andrew <wing_andrew@hotmail.com>
2021-08-13 23:58:21 -0400

    Importing mutation to create user

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +12,9 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+    // adding a user
+    const [createUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -27,16 +31,18 @@ const SignupForm = () => {
       event.stopPropagation();
     }
 
-    try {
-      const response = await createUser(userFormData);
+    try {   
+      const { data } = await createUser({
+      variables: { ...userFormData }
+    });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
       const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
