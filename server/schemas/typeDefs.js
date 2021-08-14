8da2c6f93158953fbb993fc3002fe5a7d63ac559
Andrew <wing_andrew@hotmail.com>
2021-08-14 08:50:56 -0400

    Trying to debug

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,7 @@
-const {gql} =require('apollo-server-express');
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Book{
+    type Book {
         bookId: String
         authors: [String]
         description: String
@@ -9,26 +9,33 @@ const typeDefs = gql`
         image: String
         link: String
     }
-    type User{
+    type User {
         _id: ID
         username: String
         email: String
         bookCount: Int
         savedBooks: [Book]
     }
-    type Auth{
+    type Auth {
         token: ID!
         user: User
     }
-    type Query{
+    type Query {
         me: User
-        users: [User]
     }
-    type Mutation{
+    type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User
-        removeBook(bookId: String): User
+        saveBook(book: SavedBookInput): User
+        removeBook(bookId: String!): User
+    }
+    input SavedBookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
     }
 `;
 
